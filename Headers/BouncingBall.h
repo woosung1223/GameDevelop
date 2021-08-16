90a9abb5637db5fd070f01c6cf049658c70085fa
woosung <dntjd991223khu.ac.kr>
2021-08-16 15:28:59 +0900

    Block, Ball handler class modified

--- Headers/BouncingBall.h
+++ Headers/BouncingBall.h
@@ -38,10 +38,6 @@ namespace jm
 			blockhandler = new BlockHandler;
 			ballhandler = new BallHandler;
 			ballhandler->PushBall();
-			ballhandler->PushBall();
-			ballhandler->PushBall();
-			ballhandler->PushBall();
-			ballhandler->PushBall();
 		}
 		~BouncingBall() {
 			std::cout << count << std::endl;
